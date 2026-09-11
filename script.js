2eb82c7300973fb92cb7a68ba2ead079d41207a6
Viooooonanda <vionandag@gmail.com>
2026-09-11 15:16:11 +0700

    latar

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-// Tombol 1
-document.getElementById('tombol1').addEventListener('click', function(){
-    document.getElementById('Idcard').style.display = 'block';
+// Tombol 1: Menampilkan ID Card
+document.getElementById('tombol1').addEventListener('click', function() {
+    // Ambil elemen dengan id "IdCard" lalu ubah style jadi block (muncul)
+    document.getElementById('IdCard').style.display = 'block';
 });
 
-// Tombol 2
-document.getElementById('tombol2').addEventListener('click', function(){
-    document.getElementById('Idcard').style.display = 'none'
+// Tombol 2: Menyembunyikan ID Card
+document.getElementById('tombol2').addEventListener('click', function() {
+    // Ambil elemen dengan id "IdCard" lalu ubah style jadi none (hilang)
+    document.getElementById('IdCard').style.display = 'none';
 });
