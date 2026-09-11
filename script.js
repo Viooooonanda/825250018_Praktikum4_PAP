7779be691740014af1010ca4f1835b5ceea721b7
Viooooonanda <vionandag@gmail.com>
2026-09-11 14:40:20 +0700

    buttom

---
+++ script.js
@@ -0,0 +1,9 @@
+// Tombol 1
+document.getElementById('tombol1').addEventListener('click', function(){
+    document.getElementById('Idcard').style.display = 'block';
+});
+
+// Tombol 2
+document.getElementById('tombol2').addEventListener('click', function(){
+    document.getElementById('Idcard').style.display = 'none'
+});
